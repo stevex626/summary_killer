9275d6be2f3ebe85256f0f1b6e610a35f2ed49fd
stevex626 <stevenx2021@gmail.com>
2023-09-13 22:25:02 -0700

    updates
    - Adds functionality and logic for deleting summaries from the UI.
    - Adds modal box/container for checking if the user really wants to delete the item.
    - Adds language support for CN websites.
    - Can translate text into English and respond summary in English

--- backend/server.js
+++ backend/server.js
@@ -28,22 +28,22 @@ app.post('/summarize', async (req, res) => {
             return res.status(500).json({ error: "Failed to fetch article content" });
         }
 
-        const articleContent = stdout;
+        const articleContent = stdout.toString('utf-8').trim();
         console.log(articleContent);
             // API call to openAI chat model
             try {
                 const completion = await openai.chat.completions.create({
                     messages: [{ role: 'user', content: `Please summarize the following content, and be as accurate and comprehensive as possible, and make sure to have a logical flow in your response
-                    . If it is a different language, translate the context and return the summary in the translated language. Here is the content to be summarized: ${articleContent}` }],
+                    . If it is a different language, translate the context and return the summary in English. Here is the content to be summarized: ${articleContent}` }],
                     model: 'gpt-3.5-turbo-0301',
-                    max_tokens: 750,
+                    max_tokens: 250,
                   });
                 const summary = completion.choices[0].message.content;
                 console.log(summary);
                 res.json({ summary: summary });
             } catch (summaryError) {
                 if (summaryError instanceof openAI.APIError) {
-                    res.status(summaryError.status).json({ error: `OpenAI error: ${summaryError.name}` });
+                    res.status(summaryError.status).json({ error: `OpenAI error: ${summaryError.name}, ${summaryError.message}` });
                 } else {
                     res.status(500).json({ error: `Failed to summarize due to: ${summaryError.message}` });
                 }
